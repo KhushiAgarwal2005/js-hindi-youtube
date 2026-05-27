69fe6b9552c16202efa47745a30cb9c8f87d070b
Khushi Agarwal <Khushi.btech2023@spsu.ac.in>
2026-05-27 12:46:12 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = true //undefined //null //33 //"33" //"33abc" 
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let somenumber = 33
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
