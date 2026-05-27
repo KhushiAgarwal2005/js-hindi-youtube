e4c6841d94506165158cbf61fcdb94be59edc30e
Khushi Agarwal <Khushi.btech2023@spsu.ac.in>
2026-05-27 12:10:27 +0000

    container done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //Treat all JS CODE as newer version
+
+// alter (3+3) // We are using node.js, not browser
+
+console.log(3+3);
+console.log("Khushi")
+
+
+let name = "Khushi"
+let age = 18
+let IsloggedIn = false
+
+//bignit for large no.
+
+console.log(typeof "Khushi");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undifined);
+
