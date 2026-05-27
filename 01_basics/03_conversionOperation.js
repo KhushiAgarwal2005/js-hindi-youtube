40612a911a0095028b97f08c45b4f859fa401ac3
Khushi Agarwal <Khushi.btech2023@spsu.ac.in>
2026-05-27 12:59:18 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 let score = true //undefined //null //33 //"33" //"33abc" 
 
 
@@ -16,3 +17,31 @@ let somenumber = 33
 let stringNumber = String(somenumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+// ************OPERATIONS*************** //
+
+let value =3 
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "khushi"
+let str3 = str1 + str2
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 +2);
+console.log(1 + 2 +"2");
+
